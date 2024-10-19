60e5eece24aa25ba03ce70b6e402eb7aa7697f2a
ArmandoJunior00 <armando.jr@sofintech.com.br>
2024-10-19 02:18:27 -0300

    logica somente de estrutura codicional e variaveis

---
+++ index.js
@@ -0,0 +1,62 @@
+// Classificador de nível de héroi
+
+//USAR
+/**
+ * Variaveis
+ * Operadores
+ * Laços de repetiçao
+ * estrutura de decisão
+ * 
+ * OBJETIVO
+ * crie uma variavél para armazenar o nome e a quantidade de
+ * experiencia (XP) de um héroi, depois utilize uma estrutura
+ * de decisão para apresentar alguma das mensagens abaixo:
+ * 
+ * Se XP for menor do que 1.000 = FERRO
+ * Se XP for menor do que 1.000 e 2.000 = BRONZE
+ * Se XP for menor do que 1.000 e 2.000 = PRATA
+ * Se XP for menor do que 1.000 e 2.000 = OURO
+ * Se XP for menor do que 1.000 e 2.000 = PLATINA
+ * Se XP for menor do que 1.000 e 2.000 = ASCEDENTE
+ * Se XP for menor do que 1.000 e 2.000 = IMORTAL
+ * Se XP for menor do que 1.000 e 2.000 = RADIANTE
+ * 
+ * SAIDA
+ * Ao final deve se exibir uma mensagem:
+ * O héroi de nome **{nome}** está no nivel **{nivel}** 
+ * 
+ * 
+ */
+
+let nomePersonagem = "Zelda"
+let nivelPersonagem = "platina"
+let xp = 1000
+const msg = "O héroi de nome" + nomePersonagem + " está no nivel " + nivelPersonagem
+
+switch (nivelPersonagem) {
+    case "ferro":
+        break
+
+    case "bronze":
+        break
+
+    case "prata":
+        break
+
+    case "ouro":
+        break
+
+    case "platina":
+        break
+
+    case "ascedente":
+        break
+
+    case "imortal":
+        break
+
+    case "radiante":
+        break;
+}
+
+console.log(msg)
